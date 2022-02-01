38e292ddb8a8706291020346947c1908f83348e6
Natnael Mulatu <nmulatu@unc.edu>
2022-01-31 21:43:21 -0500

    comm

---
+++ lessons/eightball.py
@@ -0,0 +1,19 @@
+"""An oracle that predicts the future"""
+
+from random import randint 
+
+input ("Ask a yes/no question: ")
+
+response: int = randint(0, 3)
+
+if response == 0:
+  print ("Most definnitely")
+  
+else response == 1:
+  print ("Highly Likely")
+  
+
+if 
+
+
+input("Ask a yes/no question: ")
